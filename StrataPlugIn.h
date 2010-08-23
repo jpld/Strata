887f6170f21e26a5993f88b67d158d4d7775abc5
jpld <imjunpyo@gmail.com>
2010-08-22 20:54:05 -0700

    initial import of skeleton project

---
+++ StrataPlugIn.h
@@ -0,0 +1,22 @@
+//
+//  StrataPlugIn.h
+//  Strata
+//
+//  Created by jpld on 22 Aug 2010.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface StrataPlugIn : QCPlugIn
+{
+}
+
+/*
+Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
