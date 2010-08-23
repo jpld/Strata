a395cefff20d5d38c9400db5333b203cb2116fba
jpld <imjunpyo@gmail.com>
2010-08-23 09:16:03 -0700

    reformat and unify QCPlugIn template code

--- StrataPlugIn.h
+++ StrataPlugIn.h
@@ -8,15 +8,5 @@
 
 #import <Quartz/Quartz.h>
 
-@interface StrataPlugIn : QCPlugIn
-{
-}
-
-/*
-Declare here the Obj-C 2.0 properties to be used as input and output ports for the plug-in e.g.
-@property double inputFoo;
-@property(assign) NSString* outputBar;
-You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
-*/
-
+@interface StrataPlugIn : QCPlugIn {}
 @end
--- StrataPlugIn.m
+++ StrataPlugIn.m
@@ -6,7 +6,6 @@
 //  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
 //
 
-/* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
 #import <OpenGL/CGLMacro.h>
 
 #import "StrataPlugIn.h"
@@ -22,99 +21,74 @@
 
 @implementation StrataPlugIn
 
-/*
-Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
-@dynamic inputFoo, outputBar;
-*/
-
-+ (NSDictionary*) attributes
-{
++ (NSDictionary*)attributes {
 	/*
 	Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
 	*/
-	
+
 	return [NSDictionary dictionaryWithObjectsAndKeys:kQCPlugIn_Name, QCPlugInAttributeNameKey, kQCPlugIn_Description, QCPlugInAttributeDescriptionKey, nil];
 }
 
-+ (NSDictionary*) attributesForPropertyPortWithKey:(NSString*)key
-{
++ (NSDictionary*)attributesForPropertyPortWithKey:(NSString*)key {
 	/*
 	Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
 	*/
-	
+
 	return nil;
 }
 
-+ (QCPlugInExecutionMode) executionMode
-{
++ (QCPlugInExecutionMode)executionMode {
 	/*
 	Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
 	*/
-	
+
 	return kQCPlugInExecutionModeProcessor;
 }
 
-+ (QCPlugInTimeMode) timeMode
-{
++ (QCPlugInTimeMode)timeMode {
 	/*
 	Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
 	*/
-	
+
 	return kQCPlugInTimeModeNone;
 }
 
-- (id) init
-{
-	if(self = [super init]) {
-		/*
-		Allocate any permanent resource required by the plug-in.
-		*/
-	}
-	
+#pragma mark -
+
+- (id)init {
+	self = [super init];
+	if(self) {
+	}	
 	return self;
 }
 
-- (void) finalize
-{
-	/*
-	Release any non garbage collected resources created in -init.
-	*/
-	
+- (void)finalize {
 	[super finalize];
 }
 
-- (void) dealloc
-{
-	/*
-	Release any resources created in -init.
-	*/
-	
+- (void)dealloc {
 	[super dealloc];
 }
 
-@end
+#pragma mark -
+#pragma mark EXECUTION
 
-@implementation StrataPlugIn (Execution)
-
-- (BOOL) startExecution:(id<QCPlugInContext>)context
-{
+- (BOOL)startExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
 	Return NO in case of fatal failure (this will prevent rendering of the composition to start).
 	*/
-	
+
 	return YES;
 }
 
-- (void) enableExecution:(id<QCPlugInContext>)context
-{
+- (void)enableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
 	*/
 }
 
-- (BOOL) execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments
-{
+- (BOOL)execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments {
 	/*
 	Called by Quartz Composer whenever the plug-in instance needs to execute.
 	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
@@ -123,19 +97,17 @@ Here you need to declare the input / output properties as dynamic as Quartz Comp
 	The OpenGL context for rendering can be accessed and defined for CGL macros using:
 	CGLContextObj cgl_ctx = [context CGLContextObj];
 	*/
-	
+
 	return YES;
 }
 
-- (void) disableExecution:(id<QCPlugInContext>)context
-{
+- (void)disableExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
 	*/
 }
 
-- (void) stopExecution:(id<QCPlugInContext>)context
-{
+- (void)stopExecution:(id<QCPlugInContext>)context {
 	/*
 	Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
 	*/
